8017225aa6c4d428f17aabef7826538253a7420a
muhammed mohsen <muhammedmohsen1111@gmail.com>
2022-10-07 05:06:01 +0200

    first

--- src/api.js
+++ src/api.js
@@ -10,7 +10,7 @@ export let getPlaceData=async(type,ne,sw)=>{
    
     },
     headers: {
-      'X-RapidAPI-Key': '43f92da55amsh54f6707ad26e8ebp12f263jsnc7a7fd7ccef1',
+      'X-RapidAPI-Key': '79b3f9c9e1msh8e55ad2195d1085p1a8965jsndd67da59f540',
       'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
     }
   
